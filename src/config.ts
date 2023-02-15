47d6f0ccc8b318b104c3bbff76b277c98d311f00
Pier Fumagalli <pier@usrz.com>
2023-02-16 00:51:35 +0100

    Strip _all_ NULLs from config

--- src/config.ts
+++ src/config.ts
@@ -33,20 +33,20 @@ const sinkValidator = object({
 })
 
 const validator = object({
-  config: optional(oneOf(configValidator, null), {}),
-  variables: optional(oneOf(object, null), {}),
-  dimensions: optional(oneOf(objectOf(string), null), {}),
+  config: optional(configValidator, {}),
+  variables: optional(object, {}),
+  dimensions: optional(objectOf(string), {}),
   probes: arrayOf(probeValidator),
   sinks: arrayOf(sinkValidator),
 })
 
 export async function parse(file: string): Promise<HostWatchDefinition> {
   const text = await readFile(file, 'utf-8')
-  const data = parseYaml(text)
-
-  const { variables, ...options } = validate(validator, data, {
-    stripOptionalNulls: true,
+  const data = parseYaml(text, {
+    reviver: (_key, val) => val == null ? undefined : val,
   })
+
+  const { variables, ...options } = validate(validator, data)
 
   const replacer = new Replacer()
   const vars = await replacer.replace(variables, true)
