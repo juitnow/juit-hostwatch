338cab2ee274dfa372d9ba4709837f35a540a290
Pier Fumagalli <pier@usrz.com>
2023-02-15 02:25:20 +0100

    Prep metrics with `RegExp`

---
+++ src/probes/regexp.ts
@@ -0,0 +1,96 @@
+import { readFile } from 'node:fs/promises'
+import { fileURLToPath } from 'node:url'
+
+import { array, object, oneOf, optional, string, url } from 'justus'
+
+import { units } from '..'
+import { AbstractProbe } from './abstract'
+
+import type { InferValidation } from 'justus'
+import type { PollData, ProbeMetrics } from './abstract'
+
+const expressionValidator = oneOf(
+    string,
+    object({
+      pattern: string({ minLength: 1 }),
+      replace: optional(string({ minLength: 1 }), '$0'),
+    }),
+)
+
+const metricValidator = object({
+  name: string({ minLength: 1 }),
+  unit: optional(oneOf(units[0], ...units.slice(1)), 'None'),
+  expr: oneOf(
+      string({ minLength: 1 }),
+      array({ items: expressionValidator, minItems: 1 }),
+  ),
+})
+
+const validator = object({
+  source: url,
+  metrics: array({ items: metricValidator, minItems: 1 }),
+})
+
+export class RegExpProbe extends AbstractProbe<ProbeMetrics, typeof validator> {
+  private _config?: InferValidation<typeof validator>
+
+  constructor() {
+    super('regex', {}, validator)
+  }
+
+  protected configure(config: InferValidation<typeof validator>): void {
+    this._config = config
+    for (const { name, unit } of config.metrics) {
+      this.metrics[name] = unit
+    }
+  }
+
+  async start(): Promise<void> {
+    await this.sample() // dry run, to check files/url are accessible
+  }
+
+  protected async sample(): Promise<PollData<ProbeMetrics>> {
+    if (! this._config) throw new Error('RegexProbe not initialized')
+
+    const { source, metrics } = this._config
+
+    let data: string
+    if (source.protocol === 'file:') {
+      const file = fileURLToPath(source)
+      data = await readFile(file, 'utf-8')
+    } else {
+      const response = await fetch(source)
+      if (response.status !== 200) {
+        throw new Error(`Error fetching ${source} (status=${response.status})`)
+      }
+      data = await response.text()
+    }
+
+    this.log.trace('Read', source)
+    this.log.trace(data)
+
+    const values: PollData<ProbeMetrics> = {}
+
+    for (const { name, expr } of metrics) {
+      let value = data
+
+      const items = typeof expr === 'string' ? [ expr ] : expr
+      for (const item of items) {
+        const { pattern, replace } = typeof item === 'string' ?
+            { pattern: item, replace: '$0' } : item
+
+        const regexp = new RegExp(pattern)
+        const match = regexp.exec(value)
+
+        value = (! match) ? '' : match.slice(0, 10)
+            .reduce((r, v = '', g) => r.replaceAll(`$${g}`, v), replace)
+
+        this.log.debug('Pattern', regexp, `(replacement="${replace}") =>`, JSON.stringify(value))
+      }
+
+      values[name] = +value
+    }
+
+    return values
+  }
+}
