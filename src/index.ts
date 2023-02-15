a852a9fa088e2a31a29afdb932f1d46fe1137fd2
Pier Fumagalli <pier@usrz.com>
2023-02-15 02:24:32 +0100

    I hate ENUMS

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,55 @@
-export enum Unit {
+import type { LogLevel } from './logger'
+
+export const Unit = {
   // Simple units
 
-  None = 'None',
+  None: 'None',
 
-  Count = 'Count',
-  Percent = 'Percent',
+  Count: 'Count',
+  Percent: 'Percent',
 
-  Microseconds = 'Microseconds',
-  Milliseconds = 'Milliseconds',
-  Seconds = 'Seconds',
+  Microseconds: 'Microseconds',
+  Milliseconds: 'Milliseconds',
+  Seconds: 'Seconds',
 
-  Bits = 'Bits',
-  Kilobits = 'Kilobits',
-  Megabits = 'Megabits',
-  Gigabits = 'Gigabits',
-  Terabits = 'Terabits',
+  Bits: 'Bits',
+  Kilobits: 'Kilobits',
+  Megabits: 'Megabits',
+  Gigabits: 'Gigabits',
+  Terabits: 'Terabits',
 
-  Bytes = 'Bytes',
-  Kilobytes = 'Kilobytes',
-  Megabytes = 'Megabytes',
-  Gigabytes = 'Gigabytes',
-  Terabytes = 'Terabytes',
+  Bytes: 'Bytes',
+  Kilobytes: 'Kilobytes',
+  Megabytes: 'Megabytes',
+  Gigabytes: 'Gigabytes',
+  Terabytes: 'Terabytes',
 
   // Rates
 
-  Count_Second = 'Count/Second',
+  Count_Second: 'Count/Second',
 
-  Bits_Second = 'Bits/Second',
-  Kilobits_Second = 'Kilobits/Second',
-  Megabits_Second = 'Megabits/Second',
-  Gigabits_Second = 'Gigabits/Second',
-  Terabits_Second = 'Terabits/Second',
+  Bits_Second: 'Bits/Second',
+  Kilobits_Second: 'Kilobits/Second',
+  Megabits_Second: 'Megabits/Second',
+  Gigabits_Second: 'Gigabits/Second',
+  Terabits_Second: 'Terabits/Second',
 
-  Bytes_Second = 'Bytes/Second',
-  Kilobytes_Second = 'Kilobytes/Second',
-  Megabytes_Second = 'Megabytes/Second',
-  Gigabytes_Second = 'Gigabytes/Second',
-  Terabytes_Second = 'Terabytes/Second',
+  Bytes_Second: 'Bytes/Second',
+  Kilobytes_Second: 'Kilobytes/Second',
+  Megabytes_Second: 'Megabytes/Second',
+  Gigabytes_Second: 'Gigabytes/Second',
+  Terabytes_Second: 'Terabytes/Second',
+} as const
+
+export type Unit = (typeof Unit)[keyof typeof Unit]
+
+export const units = Object.values(Unit)
+
+export interface Config {
+  logLevel: LogLevel,
+  logTimes: boolean,
+  logColors: boolean,
+  pollInterval: number,
 }
 
 export interface Metric {
