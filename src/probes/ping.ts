1a6ed2c5e04ce3750dcd16eb9b20eb2a4f0604cf
Pier Fumagalli <pier@usrz.com>
2023-02-15 14:08:45 +0100

    Use `millis` validator and `this.configuration` provided by super

--- src/probes/ping.ts
+++ src/probes/ping.ts
@@ -1,11 +1,11 @@
 import { createPinger } from '@juit/lib-ping'
-import { number, object, oneOf, optional, string } from 'justus'
+import { object, oneOf, optional, string } from 'justus'
 
 import { Unit } from '../index'
 import { AbstractProbe, percent } from './abstract'
+import { millis } from '../seconds'
 
-import type { Pinger, PingerOptions } from '@juit/lib-ping'
-import type { InferValidation } from 'justus'
+import type { Pinger } from '@juit/lib-ping'
 import type { PollData } from './abstract'
 
 const metrics = {
@@ -16,41 +16,32 @@ const metrics = {
 const validator = object({
   to: string({ minLength: 1 }),
   from: optional(string({ minLength: 1 })),
-  interval: optional(number({ minimum: 1000, fromString: true }), 5000),
-  timeout: optional(number({ minimum: 1000, fromString: true }), 30000),
+  interval: optional(millis({ minimum: 1000, defaultUnit: 'seconds' }), '5 sec'),
+  timeout: optional(millis({ minimum: 1000, defaultUnit: 'seconds' }), '30 sec'),
   protocol: optional(oneOf('ipv4', 'ipv6')),
 } as const)
 
 export class PingProbe extends AbstractProbe<typeof metrics, typeof validator> {
-  private _to?: string
-  private _conf?: PingerOptions
   private _pinger?: Pinger
 
   constructor() {
     super('ping', metrics, validator)
   }
 
-  protected configure(config: InferValidation<typeof validator>): void {
-    const { to, ...conf } = config
-    this._to = to
-    this._conf = conf
-  }
-
   async start(): Promise<void> {
-    if (! this._to) throw new Error('PingProbe not configured')
+    const { to, ...conf } = this.configuration
 
-    this.log.debug(`Crating pinger to "${this._to}":`, this._conf)
-    this._pinger = await createPinger(this._to, this._conf)
+    this.log.debug(`Crating pinger to "${to}":`, conf)
+    this._pinger = await createPinger(to, conf)
 
-    this._pinger.on('pong', (ms) => this.log.trace(`Pong from "${this._to}"`, ms, 'ms'))
-    this._pinger.on('error', (err) => this.log.error(`Error pinging "${this._to}"`, err))
+    this._pinger.on('pong', (ms) => this.log.trace(`Pong from "${to}"`, ms, 'ms'))
+    this._pinger.on('error', (err) => this.log.error(`Error pinging "${to}"`, err))
 
     this._pinger.start()
   }
 
   async stop(): Promise<void> {
-    if (! this._pinger) throw new Error('PingProbe not initialized')
-    await this._pinger.close()
+    if (this._pinger) await this._pinger.close()
   }
 
   protected async sample(): Promise<PollData<typeof metrics>> {
