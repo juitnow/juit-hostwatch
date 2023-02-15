9a8387fdd6866c599277c2c6fc99cc89fdb417f9
Pier Fumagalli <pier@usrz.com>
2023-02-15 02:21:59 +0100

    Prettify

--- src/sinks/console.ts
+++ src/sinks/console.ts
@@ -9,7 +9,12 @@ export class ConsoleSink extends AbstractSink<typeof never> {
     super('console', never)
   }
 
-  sink(metric: Metric): void {
-    this.log.info('Sinking metric', metric.name, '=>', metric.value, `[${metric.unit}]`)
+  sink({ name, value, unit, dimensions }: Metric): void {
+    this.log.info('Sinking metric', name, '=>', value, `[${unit}]`)
+    const length = Object.keys(dimensions).reduce((l, s) => s.length > l ? s.length : l, 0)
+    Object.entries(dimensions).forEach(([ name, value ], i, a) => {
+      const [ line, dash ] = (i + 1) == a.length ? [ ' \u2514\u2500', '\u2500' ] : [ ' \u2502 ', ' ' ]
+      this.log.info(line + `   ${name}`.padStart(length + 3, dash), '=>', value)
+    })
   }
 }
